558aeff16874638986d4f20d8c45d07d2720feeb
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-04-16 17:08:44 +0700

    Add basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,63 @@
 <script>
 	import '../app.css';
+
+	import { onMount } from 'svelte';
+
+	let canvas;
+	let ctx;
+
+	onMount(() => {
+		ctx = canvas.getContext('2d');
+		canvas.width = 500;
+		canvas.height = 500;
+		ctx.strokeStyle = '#000000';
+		ctx.lineWidth = 5;
+	});
+
+	function startDrawing(e) {
+		ctx.beginPath();
+		ctx.moveTo(e.clientX, e.clientY);
+		canvas.addEventListener('mousemove', draw);
+	}
+
+	function stopDrawing() {
+		canvas.removeEventListener('mousemove', draw);
+	}
+
+	function draw(e) {
+		ctx.lineTo(e.clientX, e.clientY);
+		ctx.stroke();
+	}
+
+	async function handleDrop(event) {
+		event.preventDefault();
+
+		const file = event.dataTransfer.files[0];
+		var reader = new FileReader();
+		reader.onload = function (event) {
+			console.log('dropped');
+			const imageObj = new Image();
+			imageObj.onload = function () {
+				ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+			};
+			imageObj.src = event.target.result;
+		};
+		reader.readAsDataURL(file);
+	}
+
+	function handleDragOver(event) {
+		event.preventDefault();
+		event.dataTransfer.dropEffect = 'copy';
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<div on:drop={handleDrop} on:dragover={handleDragOver}>Drop image here</div>
+	<canvas
+		class="border-2 border-black"
+		bind:this={canvas}
+		on:mousedown={startDrawing}
+		on:mouseup={stopDrawing}
+		on:mouseleave={stopDrawing}
+	/>
+</div>
